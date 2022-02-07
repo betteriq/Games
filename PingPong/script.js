e5471d68dbb3aa128a1170647142ef5848fb8dad
betteriq <anoushrs@gmail.com>
2022-02-07 14:50:30 -0500

    step-three

--- PingPong/script.js
+++ PingPong/script.js
@@ -2,8 +2,14 @@ import Ball from "./Ball.js";
 
 const ball = new Ball(document.getElementById("ball"));
 
+let lastTime;
+
 function update(time) {
-  console.log(time);
+  if (lastTime != null) {
+    const delta = time - lastTime;
+    console.log(delta);
+  }
+  lastTime = time;
   window.requestAnimationFrame(update);
 }
 window.requestAnimationFrame(update);
